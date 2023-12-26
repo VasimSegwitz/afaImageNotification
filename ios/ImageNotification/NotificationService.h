b64e516f514a3052568c07c4f8f3dbdd6ba8f7bc
VasimSegwitz <vasim@segwitz.com>
2023-12-26 16:05:39 +0530

    first commit

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by WEZZ_SUNNI on 20/12/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
